57ad71aa5d4b52f2638ef8850bae1a6739cbe1e6
nralif <naeemurrahman.aalif@gmail.com>
2021-05-22 18:40:00 +0600

    update to heroku deploy

--- page_project/settings.py
+++ page_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iq#+#&+!-m(b2cd7p2&og9o=wy5p4+@aa44$t++8&cpwr55g^$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
